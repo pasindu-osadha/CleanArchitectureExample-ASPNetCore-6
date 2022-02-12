3679493ad024afc493ff79d03d590825ca2c9f86
pasindu-osadha <pasiduosanda@gmail.com>
2022-02-12 17:52:09 +0530

    Add Product model on Domain layer

---
+++ CleanArchitecture.Domain/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string PName { get; set; }
+        public int Price { get; set; }
+        public string PDescription { get; set; }
+    }
+}
