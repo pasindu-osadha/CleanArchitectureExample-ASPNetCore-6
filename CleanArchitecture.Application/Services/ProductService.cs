ac6e371b983150bd159a41a560d9b746403e318d
pasindu-osadha <pasiduosanda@gmail.com>
2022-02-12 20:18:19 +0530

    implements prodcut service

---
+++ CleanArchitecture.Application/Services/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.ViewModels;
+using CleanArchitecture.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        public IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository=productRepository;
+        }
+        public ProductViewModel GetAllProduts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
