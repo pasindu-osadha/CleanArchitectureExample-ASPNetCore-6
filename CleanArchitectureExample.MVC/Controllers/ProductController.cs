ce75bd35f2055b7b46e6ee6120743704aff212cf
pasindu-osadha <pasiduosanda@gmail.com>
2022-02-12 21:26:29 +0530

    Add controller in UI Layer and call services from application layer

---
+++ CleanArchitectureExample.MVC/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitectureExample.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService=productService;
+        }
+        public IActionResult Index()
+        {
+            ProductViewModel model = _productService.GetAllProduts();
+            return View(model);
+        }
+    }
+}
