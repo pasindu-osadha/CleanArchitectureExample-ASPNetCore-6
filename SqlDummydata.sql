92514bbff2e028f6509d8ec46e30303ad0c95195
pasindu-osadha <pasiduosanda@gmail.com>
2022-02-12 21:14:11 +0530

    Add sql data script

---
+++ SqlDummydata.sql
@@ -0,0 +1,12 @@
+
+USE [Store]
+GO
+
+INSERT INTO [dbo].[Products]
+           ([PName]
+           ,[Price]
+           ,[PDescription])
+     VALUES
+           ('Lemern Puff', 100, 'raditional puff pastry biscuits sandwiched with a light lemon flavoured cream filling. Suitable for vegetarians. Suitable for vegetarians.'),
+           ('Cream cracker', '100', 'The galette Cream crackers is a flat, usually square, savoury biscuit. The name "cream crackers" refers to the method in which the mixture is creamed during manufacture.')
+GO
