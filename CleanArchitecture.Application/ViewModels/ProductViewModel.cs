bc05eb767a093509f5ccca87d6ca4483bd5b12de
pasindu-osadha <pasiduosanda@gmail.com>
2022-02-12 19:24:06 +0530

    add folders on application layer and add product view model

---
+++ CleanArchitecture.Application/ViewModels/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.ViewModels
+{
+    public class ProductViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
